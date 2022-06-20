05b2e30a9f4cfb85f0b5768e9cd2e54809169976
Harshit Kumar <harshit.kumar4@BLRETV-K6CM76KP.local>
2022-06-20 16:36:39 +0530

    added student interface

--- CRS-Group2-Flipkart-POS/src/com/flipkart/service/StudentInterface.java
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/service/StudentInterface.java
@@ -1,4 +1,10 @@
 package com.flipkart.service;
 
 public interface StudentInterface {
+    public int register(String name,String userID,String password,String gender,int batch,
+                        String branch,String address,String country);
+
+    public int getStudentId(String userId);
+
+    public boolean isApproved(int studentId);
 }
