af1873c1edf60bc850ca5aea025169b687131311
Kashish Mittal <kashish.mittal@BLRETV-R77DD63.local>
2022-06-20 11:56:18 +0530

    bean user added

--- CRS-Group2-Flipkart-POS/src/com/flipkart/bean/Student.java
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/bean/Student.java
@@ -1,4 +1,41 @@
 package com.flipkart.bean;
 
 public class Student {
+    private int studentID;
+    private String studentName;
+    private String branch;
+    private int batch;
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public int getBatch() {
+        return batch;
+    }
+
+    public void setBatch(int batch) {
+        this.batch = batch;
+    }
+
+
 }
