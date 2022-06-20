d5144329333f7cd34edfc1d12e3634c5bfbdd3cb
Kritima Kaushal <kritima.kaushal@BLRETV-CPQXFP36.local>
2022-06-20 12:43:18 +0530

    Notification Interface

---
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/service/NotificationInterface.java
@@ -0,0 +1,14 @@
+package com.flipkart.service;
+
+import com.flipkart.constant.ModeOfPayment;
+import com.flipkart.constant.NotificationType;
+
+import java.util.UUID;
+
+public interface NotificationInterface {
+
+    public int sendNotification(NotificationType type, int studentId, ModeOfPayment modeOfPayment, double amount);
+
+    public UUID getReferenceId(int notificationId);
+
+}
