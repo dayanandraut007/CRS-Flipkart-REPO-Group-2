ccf1fe13ebf8d223682749aef8a29843b8995877
Harshita Mendiratta <harshita.m@BLRETV-457HKGCY.local>
2022-06-22 01:33:06 +0530

    Updated

--- CRS-Group2-Flipkart-POS/src/com/flipkart/application/StudentCRSMenu.java
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/application/StudentCRSMenu.java
@@ -36,23 +36,15 @@ public class StudentCRSMenu {
                 case 2:
                     System.out.print("ENTER COURSE ID: ");
                     String course_code = sc.nextLine();
-<<<<<<< HEAD
-                    student.addCourse(9, course_code);
-=======
                     student.addCourse(userId, course_code);
-//                    System.out.println("Write logic for add course");
->>>>>>> d67654c233a6740ca7d5c6081c5718cab0a10b7b
+
                     break;
 
                 case 3:
                     System.out.print("ENTER COURSE ID: ");
                     course_code = sc.nextLine();
-<<<<<<< HEAD
-                    student.dropCourse(9, course_code);
-=======
                     student.dropCourse(userId, course_code);
-//                    System.out.println("Write logic for drop course");
->>>>>>> d67654c233a6740ca7d5c6081c5718cab0a10b7b
+
                     break;
 
                 case 4:
