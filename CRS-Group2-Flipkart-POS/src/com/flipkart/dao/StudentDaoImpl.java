6ad3f475b5f4ef936bbb4bdeaf1b41a30c468e65
Harshit Kumar <harshit.kumar4@BLRETV-K6CM76KP.local>
2022-06-23 08:55:57 +0530

    implemented register student and getStudentById

--- CRS-Group2-Flipkart-POS/src/com/flipkart/dao/StudentDaoImpl.java
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/dao/StudentDaoImpl.java
@@ -2,10 +2,14 @@ package com.flipkart.dao;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Student;
+import com.flipkart.constant.Role;
+import com.flipkart.constant.SQLQueriesConstants;
 import com.flipkart.utils.DBUtils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class StudentDaoImpl implements StudentDaoInterface{
@@ -26,6 +30,34 @@ public class StudentDaoImpl implements StudentDaoInterface{
 
     @Override
     public Student register(String name, String userID, String password, String gender, int batch, String branch, String address) {
+        Student stud1=new Student(userID,name,password,Role.STUDENT,Integer.parseInt(userID),branch,batch,false,address,false,false);
+        String sql = SQLQueriesConstants.STUDENT_REGISTRATION_QUERY;
+        statement =null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, userID);
+            statement.setString(2, name);
+            statement.setString(3, branch);
+            statement.setInt(4, batch);
+            statement.setString(5, address);
+            statement.setBoolean(6, false);
+            statement.setBoolean(7, false);
+            statement.setBoolean(8, false);
+
+
+            // Print query
+            System.out.println(statement.toString());
+
+            int row = statement.executeUpdate();
+            if(row == 0) {
+                System.out.println("Couldn't add the course");
+            }
+            System.out.println("Added successfully");
+
+        }
+        catch (SQLException se) {
+            se.printStackTrace();
+        }
         return null;
     }
 
@@ -35,7 +67,31 @@ public class StudentDaoImpl implements StudentDaoInterface{
     }
 
     @Override
-    public Student getStudentById(int studentId) {
+    public Student getStudentById(String userId) {
+        String sql = SQLQueriesConstants.GET_STUDENT_BY_ID_QUERY;
+        statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, userId);
+            ResultSet rs = statement.executeQuery();
+            Student st = new Student();
+            System.out.println(rs);
+            if(rs.next())
+            {
+                st.setStudentID(Integer.parseInt(rs.getString(1)));
+                st.setName(rs.getString(2));
+                st.setBranch(rs.getString(3));
+                st.setBatch(rs.getInt(4));
+                st.setAddress(rs.getString(5));
+                st.setApproved(rs.getBoolean(6));
+                st.setHasRegistered(rs.getBoolean(7));
+                st.setDonePayment(rs.getBoolean(8));
+            }
+            return st;
+        }
+        catch (SQLException se) {
+            se.printStackTrace();
+        }
         return null;
     }
 
