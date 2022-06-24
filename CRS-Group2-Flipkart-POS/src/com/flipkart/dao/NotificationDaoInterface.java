489b9a54c516390c16d79d7d27557fa071e81320
Harshita Mendiratta <harshita.m@BLRETV-457HKGCY.local>
2022-06-24 14:03:12 +0530

    Notification and Course Comments Added

--- CRS-Group2-Flipkart-POS/src/com/flipkart/dao/NotificationDaoInterface.java
+++ CRS-Group2-Flipkart-POS/src/com/flipkart/dao/NotificationDaoInterface.java
@@ -5,8 +5,29 @@ import com.flipkart.constant.NotificationType;
 
 import java.util.UUID;
 
+/**
+ * NotificationDaoInterfcae is used to implement the Notification Dao Operations
+ *
+ * @author  JEDI-June-Program-Group-2-2022
+ * @version 1.0
+ * @since   June 2022
+ */
+
 public interface NotificationDaoInterface {
+
+    /**
+     * sendNotification method is used to send the notification to user
+     * @param studentId
+     * @param notificationMessage
+     * @return {@Link boolean}
+     */
     public boolean sendNotification(String studentId, String notificationMessage);
 
+
+    /**
+     * getReferenceId used to get the reference ID of the notification
+     * @param notificationId
+     * @return {@Link UUID}
+     */
     public UUID getReferenceId(int notificationId);
 }
